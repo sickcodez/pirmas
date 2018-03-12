7a64c8a66c0a1008698a657a9f4dc397a930d7a1
Petras Butkus <p.butkus360@gmail.com>
2018-03-12 23:28:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 $(document).ready(() => {
-  $('#hl').addClass('animated bounce');
-  $('#knopke').click(() => {
-    $('#pump').toggleClass('hide');
-  });
+  var weatherApiKey = 'b517b6cbc138e320152018382e1d1fbf';
+  var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  var ipApiUrl = 'http://ip-api.com/json';
+
+  var getWeatherData = (location) => {
+    var weatherApi = weatherApiUrl + location + '&units=metric&appid=' + weatherApiKey;
+    $.getJSON(weatherApi, (weatherData) => {
+      $('#temp').append(`<h2>${weatherData.main.temp}°C</h2>`);
+      $('#kritul').append(`<h2>${weatherData.weather[0].main}</h2>`);
+      $('#loc').append(`<h3>${weatherData.sys.country}, ${weatherData.name}</h3>`);
+      console.log('Got weather data!');
+      if (weatherData.main.temp > 10) $('#temp').css('color', 'red');
+        else $('#temp').css('color', 'blue');
+    });
+  }
+
+  var getLocation = () => {
+    $.getJSON(ipApiUrl, (ipData) => {
+      getWeatherData(ipData.city);
+      console.log('Got ip-api data!');
+    });
+  }
+
+  getLocation();
+
 });
